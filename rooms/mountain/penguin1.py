1d5d9dbb0795b349a835c15dc3ce611c62f411b8
avidcoder123 <umar.tli.edu@gmail.com>
2022-02-17 16:46:57 +0000

    Make first Emperor Pinko encounter more concise

--- rooms/mountain/penguin1.py
+++ rooms/mountain/penguin1.py
@@ -4,9 +4,7 @@ from room import Room
 def scripts(player):
     io.narr("You enter a new room.")
     io.narr("It's still dark. It would have been nice if you had a flashlight, but you don't.")
-    io.narr("You hear footsteps. Who is it this time?")
-    io.narr("Shadowy figures surround you.")
-    io.narr("The figures are small, and you faintly recognize their shape.")
+    io.narr("You hear footsteps. Shadowy masses surround you.")
     io.narr("A larger figure makes its way to the forefront.")
     io.dialogue("???", "So you've finally arrived.")
     io.dialogue("???", "We didn't expect another one of you, but you just had to show your face, didn't you?")
@@ -16,25 +14,23 @@ def scripts(player):
     io.dialogue("???", "Any last words before you kick the bucket?")
     whatdoto = io.chooseList("What do you do?", ["Stand menacingly", "Say you don't want to fight"])
     if whatdoto == "Stand menacingly":
-        io.dialogue("???", "Don't try to be too tough. You're outnumbered and we will easily finish you.")
+        io.dialogue("???", "You think you're tough? Nobody's scared of a weakling like you.")
     else:
         io.dialogue("???", "You don't want to fight, Well, maybe I should just let you free...")
         io.dialogue("???", "Just kidding! You think you can get away that easily?")
         io.dialogue("???", "I'll answer that for you. You're not getting away. You're going to die.")
     io.dialogue("???", "Alright, enough blabbering. My soldiers, rage! My soldiers, fight!")
-    io.narr("You hear squawks from the crowd, and you realize that they're Pinkos.")
-    io.narr("Someone turns on the lights and you realize how outnumbered you are.")
     io.dialogue("???", "My name's Emperor Pinko. Better remember that, because it's the last name you'll ever hear!")
     io.narr("Emperor Pinko charges!")
-    io.narr("You're frozen in fear. He gets closer... and closer...")
+    io.narr("He gets closer... and closer...")
     io.narr("...And then proceeds to trip on a pebble.")
     io.dialogue("Emperor Penguin", "Ow! My flippers!")
     io.narr("There are squawks of laughter from the crowd.")
-    io.dialogue("Emperor Pinko", "Hey! Stop laughing! I'm your captain!")
+    io.dialogue("Emperor Pinko", "Stop laughing at me, you incompetent fools!")
     io.narr("You take this opportunity to escape.")
-    io.narr("You run back into the previous room and hide behind a corner.")
+    io.narr("You hide behind a corner in the last room.")
     io.dialogue("Emperor Pinko", "Where did they go? Soldiers, find them!")
-    io.narr("A horde of penguins emerge from the room to search for you.")
+    io.narr("A horde of penguins emerges from the room to search for you.")
     io.narr("They can't find you. Now's your chance.")
     escapetactic = io.chooseList("What do you do?", ["Sprint past Emperor Pinko", "Tiptoe past him like a coward"])
     if escapetactic == "Tiptoe past him like a coward":
@@ -44,14 +40,12 @@ def scripts(player):
         io.narr("You make it safely to the next room.")
     else:
         player.state["isCool"] = True
-        io.narr("You run past Emperor Pinko like the cool person you are.")
-        io.dialogue("Emperor Pinko", "HEY! YOU CAN'T JUST-")
-        io.narr("You use his face as a bouncepad, silencing him.")
-        io.narr("Being the awesome person you are, you pull off a flip in mid-air.")
-        io.narr("Big mistake. Instead of landing gracefully, you land on your face.")
-        io.narr("Ouch. That hurt.")
-        io.narr("You quickly shut the door behind you. Mission successful.")
-        io.narr("You gained 15 street cred.")
+        io.narr("You make a run for it, jumping off Emperor Pinko's face.")
+        io.narr("You do a flip in the air, attempting to land gracefully...")
+        io.narr("Only to land on your face. Ouch. That hurt.")
+        io.narr("You're now in the next room.")
+
+    #TODO: Autotransport to next room
 
 def create(player):
     map = """
@@ -72,8 +66,5 @@ def create(player):
     return Room(
         map = map,
         player = player,
-        scripts = scripts,
-        exits = {
-            "South": None
-        }
+        scripts = scripts
     )
